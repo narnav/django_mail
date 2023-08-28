a39f312f3cab0e43255b59ef04a098a77c256d91
eyalg <narnavg@gmail.com>
2023-08-28 14:00:42 +0300

    first commit

---
+++ django_template/base/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Task
+from .models import MPrint
+
+admin.site.register(MPrint)
+admin.site.register(Task)
---
+++ django_template/base/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+    def create(self, validated_data):
+        user = self.context['user']
+        print(user)
+        return Task.objects.create(**validated_data,user=user)
+
